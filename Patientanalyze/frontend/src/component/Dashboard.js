38cea3c6ad6fb8032eda831e5b6fc67ac5e51a86
devashishnyati <devashishnyati@gmail.com>
2018-11-23 23:21:01 -0800

    Dashboard UI

--- Patientanalyze/frontend/src/component/Dashboard.js
+++ Patientanalyze/frontend/src/component/Dashboard.js
@@ -101,10 +101,11 @@ class Dashboard extends Component {
                     <div class="container-fluid">
                         <Tabs defaultIndex={0}>
                             <TabList className="float-left d-flex flex-column h-100 p-5 text-white justify-content-center border rounded">
-                                <Tab className="p-3">Create/Connect to a network</Tab>
-                                <Tab className="p-3">Upload Data</Tab>
-                                <Tab className="p-3">Verify Data</Tab>
-                                <Tab className="p-3">Analyze Data</Tab><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                                <Tab className="p-3 btn">Create/Connect to a network</Tab>
+                                <Tab className="p-3 btn">Upload Data</Tab>
+                                <Tab className="p-3 btn">Verify Data</Tab>
+                                <Tab className="p-3 btn">Analyze Data</Tab>
+                                <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
                             </TabList>
                             <TabPanel>
                                 <div class="d-flex flex-column p-5 justify-content-center">
@@ -113,9 +114,9 @@ class Dashboard extends Component {
                                     <h4 class="mb-5 text-center text-white"><b>or join in an existing one!</b></h4>
                                     <div class="d-flex flex-column p-3 justify-content-center">
                                         {/* --------------------------------------------------- */}
-                                        <button type="button" class=" btn btn-md btn-warning form-control rounded" data-toggle="modal" data-target="#connectionModal">
+                                        <p class="text-center "><button type="button" class="btn btn-see text-white " data-toggle="modal" data-target="#connectionModal">
                                             Make a new Connection
-                                        </button><br></br><br></br>
+                                        </button></p><br></br><br></br>
                                         <div class="modal fade" id="connectionModal" tabindex="-1" role="dialog">
                                             <div class="modal-dialog modal-dialog-centered" role="document">
                                                 <div class="modal-content">
@@ -128,12 +129,12 @@ class Dashboard extends Component {
                                                     <div class="modal-body">
                                                         <form class="text-center">
                                                             <label>Enter the number of hospitals you want to allow to join to this network</label><br></br>
-                                                            <input type="number" onChange={this.handleChange} name="connectionRequest" placeholder="No. of participants"></input>
+                                                            <input type="number" onChange={this.handleChange} name="connectionRequest" placeholder=" No. of participants"></input>
                                                         </form>
                                                     </div>
                                                     <div class="modal-footer">
                                                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                        <button type="button" class="btn btn-warning" onClick={this.makeConnection}>Save changes</button>
+                                                        <button type="button" class="btn btn-dash" onClick={this.makeConnection}>Save changes</button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -156,9 +157,9 @@ class Dashboard extends Component {
                                     <h4 class="text-center text-white">Welcome to PatientAlyze.</h4>
                                     <h4 class="text-center text-white">Please upload your hospital's</h4>
                                     <h4 class="text-center text-white">private data to get started</h4><br></br><br></br>
-                                    <form class="w-50 h-100 border bg-grey mx-auto p-5 text-center text-white">
+                                    <form class="w-50 h-100 border bg-grey mx-auto pt-5 pb-5 text-center text-white">
                                         <p>Drop CSV file to upload</p>
-                                        <button class="btn-md btn-warning">Upload</button>
+                                        <button class="btn btn-see text-white ">Upload</button>
                                     </form>
                                 </div>
                             </TabPanel>
@@ -175,7 +176,7 @@ class Dashboard extends Component {
                                             <tr>
                                                 <td>Darshil Kapadia.csv</td>
                                                 <td>2018-09-01</td>
-                                                <td><button class="btn-sm btn-warning">Download</button></td>
+                                                <td><button class="btn-sm btn btn-dash text-white ">Download</button></td>
                                             </tr>
                                         </table>
                                     </div>
@@ -193,7 +194,7 @@ class Dashboard extends Component {
                                             <tr>
                                                 <td>Darshil Kapadia.csv</td>
                                                 <td>2018-09-01</td>
-                                                <td><button class="btn-sm btn-warning">Download</button></td>
+                                                <td><button class="btn btn-dash text-white ">Download</button></td>
                                             </tr>
                                         </table>
                                     </div>
